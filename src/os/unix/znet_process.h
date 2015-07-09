c25c07b86d61191ffa96d5fef6b3a8eb41fa87c7
zhao <zhao@localhost.localdomain>
2015-07-09 20:19:56 +0800

    add signal stop function

--- src/os/unix/znet_process.h
+++ src/os/unix/znet_process.h
@@ -6,7 +6,8 @@
 #ifndef _ZNET_PROCESS_H_INCLUDED_
 #define _ZNET_PROCESS_H_INCLUDED_
 
-int znet_init_signals(void);
+znet_int_t znet_init_signals(void);
+znet_int_t znet_os_signal_process(char *sig, znet_int_t pid);
 
 typedef pid_t znet_pid_t;
 
