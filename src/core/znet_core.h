9a158d15a76668f2d93c955e81c9f74599d31acf
zhao <HITsZhao@163.com>
2015-08-16 22:32:27 +0800

    add queue struct files

--- src/core/znet_core.h
+++ src/core/znet_core.h
@@ -42,6 +42,7 @@ typedef void (*znet_connection_handler_pt)(znet_connection_t *c);
 #include<znet_string.h>
 #include<znet_array.h>
 #include<znet_list.h>
+#include<znet_queue.h>
 #include<znet_setproctitle.h>
 #include<znet_socket.h>
 #include<znet_event.h>
